44303a8d1568198883353a7c173f4c666218ad7c
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2015-11-10 20:17:48 +0100

    Added gulp auto config to the gilpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,21 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+//elixir(function(mix) {
+//    mix.sass('app.scss');
+//});
+
+var gulp = require ('gulp');
+var browserify = require('gulp-browserify');
+var rename = require('gulp-rename');
+
+gulp.task('js', function() {
+    gulp.src('public/js/main.js')
+        .pipe(browserify())
+        .pipe(rename('public/js/bundle.js'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('public/js/**/*.js')
 });
